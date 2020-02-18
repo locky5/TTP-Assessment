798dd571a20265ad2ca0e124e62ae092644150e1
locky5 <daikevin19@yahoo.com>
2020-02-18 03:50:43 -0500

    finished backend for users

--- TTP-backend/ttpbackend/config/routes.rb
+++ TTP-backend/ttpbackend/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  
+  get "/auto_login", to: "auth#auto_login"
+  post "/signup", to: "users#create"
+  post "/login", to: "auth#login"
 end
